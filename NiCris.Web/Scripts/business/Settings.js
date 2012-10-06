81dc808b18fb947d2b65b71cc04cc05050014f1e
BorisMomtchev@g.com <BorisMomtchev@g.com>
2012-10-06 08:47:15 -0500

    Add Notifications

--- NiCris.Web/Scripts/business/Settings.js
+++ NiCris.Web/Scripts/business/Settings.js
@@ -10,6 +10,8 @@ Settings.init = function () {
     $("#btnSuccess").click(Settings.success);
     $("#btnWarning").click(Settings.warning);
     $("#btnDanger").click(Settings.error);
+
+    $("#btnInverse").click(Settings.inverse);
 }
 
 
@@ -32,6 +34,25 @@ function newAlert(type, message) {
 }
 
 
+
+Settings.inverse = function () {
+    // newNotification('.top-right', 'danger', 'Some Cool Danger!');
+    // newNotification('.bottom-right', 'info', 'Some Cool Info!');
+    // newNotification('.top-right', 'bangTidy', 'Some Cool bangTidy!');
+    newNotification('.top-right', 'blackgloss', 'Some Cool blackgloss!');
+}
+
+function newNotification(location, type, message) {
+    $(location).notify({
+        type: type,
+        message: { text: message }
+    }).show();
+}
+
+
+
+
+
 // Original Code
 function newAlertOriginalCode(type, message) {
     $("#alert-area").append($("<div class='alert-message " + type + " fade in' data-alert><p> " + message + " </p></div>"));
@@ -41,3 +62,6 @@ function newAlertOriginalCode(type, message) {
 // http://stackoverflow.com/questions/7676356/can-twitter-bootstrap-alerts-fade-in-as-well-as-out
 // http://twitter.github.com/bootstrap/base-css.html#buttons
 // http://twitter.github.com/bootstrap/javascript.html#alerts
+
+// http://nijikokun.github.com/bootstrap-notify/
+// http://www.w3resource.com/twitter-bootstrap/alerts-and-errors-tutorial.php
